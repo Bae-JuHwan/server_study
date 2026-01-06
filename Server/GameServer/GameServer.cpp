acfb369690a6129709fa31cd0dc12324a9dac4e1
unknown <0412juhwan@gmail.com>
2026-01-06 18:01:34 +0900

    10

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -4,42 +4,61 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include "AccountManager.h"
-#include "UserManager.h"
 
-void Fun1()
+class SpinLock
 {
-	for (int32 i = 0; i < 1000; i++)
+public:
+	void lock()
 	{
-		UserManager::Instance()->ProcessSave();
+		// CAS (Compare-And-Swap)
+
+		bool expected = false;
+		bool desired = true;
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+		}
+	}
+
+	void unlock()
+	{
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add()
+{
+	for (int32 i = 0; i < 100000; i++)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum++;
 	}
 }
 
-void Fun2()
+void Sub()
 {
-	for (int32 i = 0; i < 1000; i++)
+	for (int32 i = 0; i < 100000; i++)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
 	}
 }
 
 int main()
 {	
-	std::thread t1(Fun1);
-	std::thread t2(Fun2);
+	thread t1(Add);
+	thread t2(Sub);
 
 	t1.join();
 	t2.join();
 
-	cout << "Jobs Done" << endl;
-
-	// 참고
-	mutex m1;
-	mutex m2;
-
-	std::lock(m1, m2);
-
-	// adopt lock : 이미 lock된 상태니까, 나중에 소멸될때 풀어주기만 해
-	lock_guard<mutex> g1(m1, std::adopt_lock);
-	lock_guard<mutex> g2(m2, std::adopt_lock);
+	cout << sum << endl;
 }
