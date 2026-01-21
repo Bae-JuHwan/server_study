ba62b1f1b7ace3040530b092cfb23e956e6ff05f
unknown <0412juhwan@gmail.com>
2026-01-21 23:28:47 +0900

    24

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -5,40 +5,25 @@
 #include <mutex>
 #include <future>
 #include <Windows.h>
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
+#include "CoreMacro.h"
+#include "ThreadManager.h"
 
-LockFreeQueue<int32> q;
-LockFreeStack<int32> s;
+CoreGlobal core;
 
-void Push()
+void ThreadMain()
 {
 	while (true)
 	{
-		int32 value = rand() % 100;
-		q.Push(value);
-
-		this_thread::sleep_for(1ms);
-	}
-}
-
-void Pop()
-{
-	while (true)
-	{
-		auto data = q.TryPop();
-		if (data != nullptr)
-			cout << (*data) << endl;
+		cout << "Hello I am thread... " << LThreadId << endl;
+		this_thread::sleep_for(1s);
 	}
 }
 
 int main()
 {
-	thread t1(Push);
-	thread t2(Pop);
-	thread t3(Pop);
-
-	t1.join();
-	t2.join();
-	t3.join();
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadMain);
+	}
+	GThreadManager->Join();
 }
