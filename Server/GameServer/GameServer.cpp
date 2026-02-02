3f23436715cd7bdecfb1b6326aeca2aab6b5245e
unknown <0412juhwan@gmail.com>
2026-02-03 00:43:48 +0900

    37

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -11,33 +11,83 @@
 #include "Memory.h"
 #include "Allocator.h"
 
-class Knight
+using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
+
+class Player
 {
 public:
-	int32 _hp = rand() % 1000;
+	Player()
+	{
+		INIT_TL(Player);
+	}
+	virtual ~Player() {}
+
+	DECLARE_TL
 };
 
-class Monster
+class Knight : public Player
 {
 public:
-	int64 _id = 0;
+	Knight() { INIT_TL(Knight); }
+};
+
+class Mage : public Player
+{
+public:
+	Mage() { INIT_TL(Mage); }
+};
+
+class Archer : public Player
+{
+public:
+	Archer() { INIT_TL(Archer); }
+};
+
+class Dog
+{
+
 };
 
 int main()
 {
-	Knight* knights[100];
+	//TypeList<Mage, Knight>::Head whoAmI;
+	//TypeList<Mage, Knight>::Tail whoAmI2;
 
-	for (int32 i = 0; i < 100; i++)
-		knights[i] = ObjectPool<Knight>::Pop();
+	//TypeList<Mage, TypeList<Knight, Archer>>::Head whoAmI3;
+	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAmI4;
+	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAmI5;
 
-	for (int32 i = 0; i < 100; i++)
+	//int32 len1 = Length<TypeList<Mage, Knight>>::value;
+	//int32 len2 = Length<TypeList<Mage, Knight, Archer>>::value;
+
+
+	//TypeAt<TL, 0>::Result whoAmI6;
+	//TypeAt<TL, 1>::Result whoAmI7;
+	//TypeAt<TL, 2>::Result whoAmI8;
+
+	//int32 index1 = IndexOf<TL, Mage>::value;
+	//int32 index2 = IndexOf<TL, Archer>::value;
+	//int32 index3 = IndexOf<TL, Dog>::value;
+
+	//bool canConvert1 = Conversion<Player, Knight>::exists;
+	//bool canConvert2 = Conversion<Knight, Player>::exists;
+	//bool canConvert3 = Conversion<Knight, Dog>::exists;
+
+	//{
+	//	Player* player = new Knight();
+
+	//	bool canCast = CanCast<Knight*>(player);
+	//	Knight* knight = TypeCast<Knight*>(player);
+
+	//	delete player;
+	//}
+
 	{
-		ObjectPool<Knight>::Push(knights[i]);
-		knights[i] = nullptr;
+		shared_ptr<Knight> knight = MakeShared<Knight>();
+		
+		shared_ptr<Player> player = TypeCast<Player>(knight);
+		bool canCast = CanCast<Player>(knight);
 	}
-
-	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
-	shared_ptr<Knight> sptr2 = MakeShared<Knight>();
 
 	for (int32 i = 0; i < 5; i++)
 	{
@@ -45,13 +95,7 @@ int main()
 			{
 				while (true)
 				{
-					Knight* knight = xnew<Knight>();
 
-					cout << knight->_hp << endl;
-
-					this_thread::sleep_for(10ms);
-
-					xdelete(knight);
 				}
 			});
 	}
