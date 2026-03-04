2696203ddeb55ff2295d9680ecee4f87d529a594
unknown <0412juhwan@gmail.com>
2026-03-05 03:00:32 +0900

    68

--- Server/DummyClient/ServerPacketHandler.h
+++ Server/DummyClient/ServerPacketHandler.h
@@ -4,42 +4,41 @@
 using PacketHandlerFunc = std::function<bool(PacketSessionRef&, BYTE*, int32)>;
 extern PacketHandlerFunc GPacketHandler[UINT16_MAX];
 
-// TODO : 자동화
 enum : uint16
 {
-	PKT_S_TEST = 1,
-	PKT_S_LOGIN = 2,
+	PKT_C_TEST = 1000,
+	PKT_C_MOVE = 1001,
+	PKT_S_TEST = 1002,
+	PKT_S_LOGIN = 1003,
 };
 
-// TODO : 자동화
 // Custom Handlers
 bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len);
-bool Handle_S_TEST(PacketSessionRef& session, Protocol::S_TEST& pkt);
+bool Handle_S_TEST(PacketSessionRef& session, Protocol::S_TEST&pkt);
+bool Handle_S_LOGIN(PacketSessionRef& session, Protocol::S_LOGIN&pkt);
 
 class ServerPacketHandler
 {
 public:
-	// TODO : 자동화
 	static void Init()
 	{
-		for (int32 i = 0; i < UINT64_MAX; i++)
+		for (int32 i = 0; i < UINT16_MAX; i++)
 			GPacketHandler[i] = Handle_INVALID;
-
-		GPacketHandler[PKT_S_TEST] = [](PacketSessionRef& session, BYTE* buffer, int32 len) { return HandlePacket<Protocol::S_TEST>(Handle_S_TEST, session, buffer, len);};
+		GPacketHandler[PKT_S_TEST] = [](PacketSessionRef& session, BYTE* buffer, int32 len) { return HandlePacket < Protocol::S_TEST > (Handle_S_TEST, session, buffer, len); };
+		GPacketHandler[PKT_S_LOGIN] = [](PacketSessionRef& session, BYTE* buffer, int32 len) { return HandlePacket < Protocol::S_LOGIN > (Handle_S_LOGIN, session, buffer, len); };
 	}
 
-	static bool HandlePacket(PacketSessionRef& session, BYTE* buffer, int32 len)
+	static bool HandlePacket(PacketSessionRef & session, BYTE * buffer, int32 len)
 	{
 		PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
 		return GPacketHandler[header->id](session, buffer, len);
 	}
-
-	// TODO : 자동화
-	static SendBufferRef MakeSendBuffer(Protocol::S_TEST& pkt) { return MakeSendBuffer(pkt, PKT_S_TEST); };
+	static SendBufferRef MakeSendBuffer(Protocol::C_TEST&pkt) { return MakeSendBuffer(pkt, PKT_C_TEST); }
+	static SendBufferRef MakeSendBuffer(Protocol::C_MOVE&pkt) { return MakeSendBuffer(pkt, PKT_C_MOVE); }
 
 private:
 	template<typename PacketType, typename ProcessFunc>
-	static bool HandlePacket(ProcessFunc func, PacketSessionRef& session, BYTE* buffer, int32 len)
+	static bool HandlePacket(ProcessFunc func, PacketSessionRef & session, BYTE * buffer, int32 len)
 	{
 		PacketType pkt;
 		if (pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)) == false)
@@ -49,7 +48,7 @@ private:
 	}
 
 	template<typename T>
-	static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
+	static SendBufferRef MakeSendBuffer(T & pkt, uint16 pktId)
 	{
 		const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong());
 		const uint16 packetSize = dataSize + sizeof(PacketHeader);
